f9d4ca9c52e6a8cc460b623f850a98e0f18c9363
Daniel Nalborczyk <dnalborczyk@gmail.com>
2020-04-26 09:58:09 -0400

    Fix build scripts

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   modulePathIgnorePatterns: [
-    './packages/serverless-local-handler-lambda/src/__tests__',
+    // './packages/serverless-local-handler-lambda/src/__tests__',
     './src/lambda/__tests__',
     './src/utils/__tests__',
     './tests',

04c0dc952d7a2ebd633ce6974ee8d872fe1b3ab2
Daniel Nalborczyk <dnalborczyk@gmail.com>
2020-04-26 18:00:28 -0400

    Fix husky script

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@
 module.exports = {
   hooks: {
     'pre-commit': 'npm run lint && npm run prettier',
-    'pre-push': 'npm run lint && npm run prettier && npm build && npm test',
+    'pre-push': 'npm run lint && npm run prettier && npm run build && npm test',
   },
 }
